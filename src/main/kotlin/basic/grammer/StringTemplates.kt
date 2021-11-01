90a6432b3b5162bb8dd5039e1520b5442d24bfee
yisong.li <yisong.li@reece.com.au>
2021-11-01 11:24:01 +0800

    test string

---
+++ src/main/kotlin/basic/grammer/StringTemplates.kt
@@ -0,0 +1,18 @@
+package basic.grammer
+
+class StringTemplates {
+
+    fun printFormat(s: String): String {
+        return "Format: $s"
+    }
+
+    fun printFormatWithStringFunction(s: String): String {
+        return "Format: ${s.uppercase()}"
+    }
+}
+
+fun main() {
+    val stringTemplates = StringTemplates()
+    println(stringTemplates.printFormat("Hello String"))
+    println(stringTemplates.printFormatWithStringFunction("hello string"))
+}
