40d8e169adc74ad3cb45ee0f28e9b75fb30a876f
yisong.li <yisong.li@reece.com.au>
2021-11-01 13:55:50 +0800

    test when expression

---
+++ src/main/kotlin/basic/grammar/WhenExpression.kt
@@ -0,0 +1,27 @@
+package basic.grammar
+
+class WhenExpression {
+
+    fun whenExpression(obj: Any): String {
+        return when (obj) {
+            1 -> "One"
+            "Hello" -> "Get Hello"
+            is Long -> "Is Long"
+            !is String -> "Is not String"
+            else -> "Unknown"
+        }
+    }
+}
+
+fun main() {
+    printResult(1, 1)
+    printResult(2, "Hello")
+    printResult(3, 1L)
+    printResult(4, listOf(1))
+    printResult(5, 4.0)
+}
+
+private fun printResult(number: Int,obj: Any) {
+    val whenExpression = WhenExpression()
+    println(number.toString() + "  " + whenExpression.whenExpression(obj))
+}
