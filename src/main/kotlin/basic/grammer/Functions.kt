4f172de87cb0980c96e4d42e3cb48aa39e045d4f
yisong.li <yisong.li@reece.com.au>
2021-11-01 11:02:31 +0800

    test functions

---
+++ src/main/kotlin/basic/grammer/Functions.kt
@@ -0,0 +1,14 @@
+package basic.grammer
+
+fun sumReturn(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun sumNotReturn(x: Int, y: Int) {
+    println(x + y)
+}
+
+fun main(args: Array<String>) {
+    println(sumReturn(5, 10))
+    sumNotReturn(10, 10)
+}
