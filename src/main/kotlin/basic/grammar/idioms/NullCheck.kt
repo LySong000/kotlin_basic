ea11653fcacf053c8798c4eae45211f76580d4c6
yisong.li <yisong.li@reece.com.au>
2021-11-02 14:46:07 +0800

    [idioms]test execute if not null

--- src/main/kotlin/basic/grammar/idioms/NullCheck.kt
+++ src/main/kotlin/basic/grammar/idioms/NullCheck.kt
@@ -13,10 +13,16 @@ class NullCheck {
         val listFiles = File("Test").listFiles()
         println(listFiles?.size ?: "I am null")
     }
+
+    fun executeIfNotNull() {
+        val value = null
+        value?.let { println("I am not null") }
+    }
 }
 
 fun main() {
     val nullCheck = NullCheck()
     nullCheck.notNullShorthand()
     nullCheck.notNullElse()
+    nullCheck.executeIfNotNull()
 }
