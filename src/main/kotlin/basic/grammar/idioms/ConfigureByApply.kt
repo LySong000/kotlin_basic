b6e8fda4dcdab91c545e7e7286765ad9b45c11dd
yisong.li <yisong.li@reece.com.au>
2021-11-04 10:29:11 +0800

    [idioms]i don't know why it can not fucking running

---
+++ src/main/kotlin/basic/grammar/idioms/ConfigureByApply.kt
@@ -0,0 +1,16 @@
+package basic.grammar.idioms
+
+/**
+ * wtf
+ */
+class ConfigureByApply() {
+
+}
+
+//fun main() {
+//    val configureByApply = ConfigureByApply().apply {
+//        var age = 20
+//        var city = "Xi'an"
+//    }
+//    println(configureByApply)
+//}
